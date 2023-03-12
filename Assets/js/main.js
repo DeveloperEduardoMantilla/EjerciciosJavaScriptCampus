b4adc130f72dd6c8c683a1bdf779d1fdd0e7fd9c
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-03-11 23:41:59 -0500

    feat(EjerciciosJavaScript): :sparkles: Ejercicio 8
    Se desarolla solucion para ejercicio #8 propuesto en el pdf - campus

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,15 +1,26 @@
-//Ejercicio 7
+//Ejercicio 8
 /* 
-Programa que pida el ingreso del nombre y precio de un artículo y la
-cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
-en su factura.*/
-
-let nombre="";
-let precio="";
-let cantidad="";
-
-nombre=prompt("Por favor digite el nombre del producto");
-precio=parseFloat(prompt("Por favor digite el precio del producto"));
-cantidad=parseInt(prompt("Digite la cantidad de productos"));
-
-console.log(`Usted tiene que pagar un valor de ${cantidad*precio} por el producto ${nombre}, el cual lleva una cantidad de ${cantidad} productos.`)
+Programa que Ingrese por teclado:
+a. el valor del lado de un cuadrado para mostrar por pantalla el
+perímetro del mismo
+b. la base y la altura de un rectángulo para mostrar el área del
+mismo*/
+do{
+    let opcion = prompt(`Por favor seleccion que desea hacer
+    a. Hallar perimetro de un cuadrado
+    b. Area de un triangulo`)
+    switch (opcion) {
+        case 'a':
+            let perimetro=parseInt(prompt("Digite la medida de uno de los lados del cuadrado"));
+            console.log("El perimetro del cuadrado es: " +(perimetro*4))
+            break;
+        case 'b':
+            let base=parseInt(prompt("Digite la base del triangulo"));
+            let altura=parseInt(prompt("Digite la altura del triangulo"));
+            console.log("El area del triangulo es:" +((base*altura)/2));
+            break;
+        default:
+            console.log("Opcion Incorrecta")
+            break;
+    }
+}while(confirm("¿Desea realizar otra consulta?"))
