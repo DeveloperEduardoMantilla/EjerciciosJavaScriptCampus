48595329aae179cc9a3723cd9a5a395e304f6e97
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-03-11 21:18:12 -0500

    feat(EjerciciosJavaScript): :sparkles: Ejercicio 3
    Se desarolla solucion para ejercicio #3 propuesto en el pdf - campus

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,19 +1,11 @@
-//Ejercicio 2
-/* Dado un número indicar si es par o impar y si es mayor de 10. */
-let respuesta=" ";
-let numero=0
-do {
-    numero = parseInt(prompt('Digite un numero para saber si par o impar', 5))
-    if (numero % 2 == 0) {
-        respuesta="";
-        respuesta+='El numero ' + numero + ' es par';
-        numero>9?respuesta+=" y es mayor a 10":respuesta+=" y es menor a 10";
-        console.log(respuesta)
-    } else {
-        respuesta="";
-        respuesta+='El numero ' + numero + ' es impar';
-        numero>9?respuesta+=" y es mayor a 10":respuesta+=" y es menor a 10";
-        console.log(respuesta)
-    }
-} while (confirm('Desea revisar otro numero?'))
+//Ejercicio 3
+/* 
+Construir el algoritmo para determinar el voltaje de un
+circuito a partir de la resistencia y la intensidad de corriente. */
 
+let resistencia = parseInt(prompt('Digite la resistencia'))
+let intensidad = parseInt(prompt('Digite la intensidad'))
+
+let voltaje = resistencia * intensidad
+
+console.log('El voltaje del circuito es: ' + voltaje + ' Ohmio')
