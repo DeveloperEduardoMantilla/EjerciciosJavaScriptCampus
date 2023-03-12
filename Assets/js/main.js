362d0fe9adfdc1b76f42681f662f7819930a05b9
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-03-11 21:48:03 -0500

    feat(EjerciciosJavaScript): :sparkles: Ejercicio 4
    Se desarolla solucion para ejercicio #4 propuesto en el pdf - campus

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,11 +1,39 @@
-//Ejercicio 3
+//Ejercicio 4
 /* 
-Construir el algoritmo para determinar el voltaje de un
-circuito a partir de la resistencia y la intensidad de corriente. */
+Construir el algoritmo que solicite el nombre y edad de 3
+personas y determine el nombre de la persona con mayor edad. */
 
-let resistencia = parseInt(prompt('Digite la resistencia'))
-let intensidad = parseInt(prompt('Digite la intensidad'))
+let datos = []
 
-let voltaje = resistencia * intensidad
+function persona(id,nombre, edad){
+    this.id=id;
+    this.nombre= nombre;
+    this.edad=edad;
+}
 
-console.log('El voltaje del circuito es: ' + voltaje + ' Ohmio')
+for (let i=0; i <3; i++) {
+    let nombre=prompt("Por favor escriba su nombre");
+    let edad=parseInt(prompt("Por favor escriba su edad"));
+    let per = new persona(i,nombre,edad);
+    datos.push(per);
+}
+mostrarListado();
+
+function mostrarListado(){
+    let lista='';
+    let edad=0;
+    let resultado= "";
+    for(let i=0; i<datos.length; i++){
+        lista+=
+            'id: ' + datos[i].id +
+            ' nombre: ' + datos[i].nombre +
+            ' edad: ' + datos[i].edad + '\n'; 
+        if(edad<datos[i].edad){
+            edad=datos[i].edad;
+            resultado="El jugador " + datos[i].nombre + " es el jugador con mayor edad, su edad es "+ datos[i].edad;
+        }     
+    }
+    console.log('=======Lista de Jugadores=======');
+    console.log(lista);
+    console.log(resultado);
+  }
